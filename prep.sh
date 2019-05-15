655c12381521d27e67f08411a80c590f3b337835
Jason Lee <42029975+JasonLee925@users.noreply.github.com>
2019-05-15 22:30:34 +0800

    add init command

--- prep.sh
+++ prep.sh
@@ -119,5 +119,17 @@ main() {
 		echo "Please reboot."
 		exit 0
 	fi
+	
+	
+	echo "Start initial cluster"
+	wifiIP = $(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+	sudo kubeadm init --pod-network-cidr=10.244.10.0/16 --apiserver-advertise-address=$wifiIP 
+	mkdir -p $HOME/.kube
+	sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+	sudo chown $(id -u):$(id -g) $HOME/.kube/config
+	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+	
+	echo "Please reboot."
+	
 }
 main
