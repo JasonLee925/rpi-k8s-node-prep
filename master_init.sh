d3ddaec6af395c1acd8955de0a460496634e00ba
Jason Lee <42029975+JasonLee925@users.noreply.github.com>
2019-05-16 14:22:27 +0800

    Update master_init.sh

--- master_init.sh
+++ master_init.sh
@@ -1,5 +1,5 @@
 echo "Start initial cluster"
-wifiIP = $(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+wifiIP=$(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 sudo kubeadm init --pod-network-cidr=10.244.10.0/16 --apiserver-advertise-address=$wifiIP 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
